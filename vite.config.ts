c6651b1d5bd8f79aa0df6d290b501f68adea83b8
zodo15 <hitanshulokeshparekh1511@gmail.com>
2025-02-23 16:30:38 +0530

    tested build

--- vite.config.ts
+++ vite.config.ts
@@ -23,9 +23,7 @@ export default defineConfig(({ mode }) => ({
     sourcemap: true,
     outDir: 'dist',
     rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html')
-      },
+      input: path.resolve(__dirname, 'index.html'),
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],
